0f5a9ea004d1e3fa1bc4eeff12b7b3a00f1a660f
Aliyu <aliyubababello@gmail.com>
2022-08-22 10:02:34 +0100

    dotenv added

--- main.py
+++ main.py
@@ -10,7 +10,9 @@ import jwt
 
 # pylint: disable=import-error
 from flask import Flask, jsonify, request, abort
+from dotenv import load_dotenv
 
+load_dotenv()
 
 JWT_SECRET = os.environ.get('JWT_SECRET', 'abc123abc1234')
 LOG_LEVEL = os.environ.get('LOG_LEVEL', 'INFO')
